4bd8483d2d4ad0240673a02efde569e552088696
Jimmy Satterfield <fedora.game.dev@gmail.com>
2021-06-11 11:42:33 -0700

    Added saveRoutineObject to LoadAsync interface

--- Public/ISceneLoader.cs
+++ Public/ISceneLoader.cs
@@ -4,5 +4,5 @@ using UnityEngine;
 public interface ISceneLoader
 {
     void Load(ISceneObject sceneObject);
-    IEnumerator LoadAsync(ISceneObject sceneObject, GameObject routineObject);
+    IEnumerator LoadAsync(ISceneObject sceneObject, GameObject routineObject, bool saveRoutineObject);
 }
