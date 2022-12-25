506b1409eef8e27d941fc3714e3f529d0ec94278
MochaMoth <Fedora.Game.Dev@gmail.com>
2022-12-24 18:23:01 -0800

    Fix Player-Side SceneReference

--- Implementations/SceneReferencePlayer.cs
+++ Implementations/SceneReferencePlayer.cs
@@ -6,14 +6,13 @@ namespace FedoraDev.SceneObject.Implementations
     {
         [SerializeField] string _scenePath = string.Empty;
 
-#if UNITY_EDITOR
-#else
-        public string GetScenePath(Object sceneAsset) => return _scenePath;
+#if !UNITY_EDITOR
+        public string GetScenePath(Object sceneAsset) => _scenePath;
         public bool IsValidSceneAsset(Object sceneAsset) => true;
         
         public void SetScenePath(string value) { }
         public void OnBeforeSerialize(Object sceneAsset) { }
         public void OnAfterDeserialize(Object sceneAsset) { }
 #endif
-    }
+	}
 }
